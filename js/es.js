8ba73386a89fd8000494a47e2049b569550e3d88
igor <perrofeofilms@gmail.com>
2020-01-06 11:11:48 -0600

    pagar mas tarde info

---
+++ js/es.js
@@ -0,0 +1,6 @@
+Snipcart.execute('bind', 'app.ready', function() {
+    Snipcart.execute('registerLocale', 'es', {
+    "payment_informations_bill_me_later":
+    "Realiza el pago por transferencia a las siguientes cuentas: (Ponemos numero de cuentas). Envía el comprobante de pago al correo info@healthypleasurescr.com"
+    });
+});
